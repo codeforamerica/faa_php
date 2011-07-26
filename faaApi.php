620a2a253d58d63792312c53c81b6e1c61d7cf31
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-07-26 14:03:40 -0700

    removed preg match

--- faaApi.php
+++ faaApi.php
@@ -12,7 +12,7 @@ class faaApi extends APIBaseClass{
 	public function airport_status($airportCode,$format='json'){
 	// airport code is three letters
 	// more info at http://services.faa.gov/docs/services/airport/
-		return ( preg_match("[A-Za-z]{3}",$airportCode) ? self::_request($path."/airport/status/$airportCode", 'get', array('format'=>"application/$format"), "Accept: application/$format"):false);
+		return ( self::_request($path."/airport/status/$airportCode", 'get', array('format'=>"application/$format"), "Accept: application/$format"));
 	}
 	
 }
